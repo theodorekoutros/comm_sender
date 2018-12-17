8c8aa023f45228e5e2b081b5798af080ec4c8a61
cbteeple <cbteeple@g.harvard.edu>
2018-12-17 11:41:00 -0500

    added conversion and subscription from master control nodes.

---
+++ src/act2serial.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def convert(in_array):
+
+	out_str = 'set'
+	out_str += ';0'
+
+	for element in in_array:
+		out_str += str(element);
+		out_str += ';'
+
+	out_str += ';0'
+	out_str += ';0'
+
+
+	return out_str
--- src/comm_sender_node.py
+++ src/comm_sender_node.py
@@ -6,6 +6,8 @@ import time
 import sys, select, termios, tty
 from std_msgs.msg import String
 from comm_sender.msg import Comm
+from gantry_control.msg import *
+import act2serial
 
 devname='/dev/ttyACM1'
 
@@ -15,19 +17,27 @@ class CommSenderNode:
 
         rospy.loginfo("[%s] Initialzing." %(self.node_name))     
         
-        self.key = None
+        self.sendStr = None
 
         self.s = serial.Serial(devname)
  
-        self.sub_key = rospy.Subscriber("/key", Comm, self.cbKey, queue_size=1)
-        self.rate = rospy.Rate(10) # 30hz 
+        self.sub_key = rospy.Subscriber("/key", Comm, self.cbKey, queue_size=10)
+        self.sub_actuation = rospy.Subscriber("/gantry/set_actuation", actuation, self.cbActuation, queue_size=10)
+        self.rate = rospy.Rate(50) # 30hz 
+
+    def sendSer(self)
+        if self.sendStr != '':
+            rospy.loginfo("Sending.[%s] to serial. " %(self.sendStr))
+            self.s.write(self.sendStr+'\n')
 
     def cbKey(self, msg):
-        self.key = msg.data
+        self.sendStr = msg.data
+        self.sendSer()
                
-        if self.key != '':
-            rospy.loginfo("Sending.[%s] to serial. " %(self.key))
-            self.s.write(self.key)
+
+    def cbActuation(self,msg):
+        self.sendStr = act2serial.convert(msg.levels)
+        self.sendSer()
 
 if __name__=="__main__":
     rospy.init_node('comm_sender_node', anonymous=False)
